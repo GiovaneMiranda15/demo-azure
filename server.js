a711878125803ee1db3bc80d56d81fab38a3db3a
Giovane Miranda <mirandagiovane7@gmail.com>
2025-03-14 10:12:07 -0400

    atualizando api

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const app = require('./config/express')();
-const port = app.get('port')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
